37adbfe07e88b75c63663583aeaab161d2e453a6
poolsiderobot <roboticpurple44@gmail.com>
2023-11-13 16:47:39 +0530

    Media Files

---
+++ app/src/main/java/com/example/quikflash/StudyActivity.java
@@ -0,0 +1,14 @@
+package com.example.quikflash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study);
+    }
+}
